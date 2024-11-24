9ad6f4d2385123cb444c096032728bf823e17256
Charles Gama <suelagama@gmail.com>
2024-11-24 11:27:55 -0300

    Update .gtiignore  / --cached

--- library/utils/generate_secret_key.py
+++
@@ -1,5 +0,0 @@
-from django.core.management.utils import get_random_secret_key
-
-# Gera uma chave secreta usando o Django
-secret_key = get_random_secret_key()
-print(secret_key)
--- library/views/__init__.py
+++
@@ -1,7 +0,0 @@
-from .author import *
-from .book import *
-from .category import *
-from .dashboard import *
-from .loan import *
-from .publisher import *
-from .student import *
